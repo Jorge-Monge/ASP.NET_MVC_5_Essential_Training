b481f087616eb52b5861a36ad2616d973ad28d28
Jordimonk <jordimonk@yahoo.com>
2017-12-01 22:27:25 -0700

    First commit of the AutomatedTellerMachine example

---
+++ AutomatedTellerMachine/AutomatedTellerMachine/App_Start/RouteConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace AutomatedTellerMachine
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+              name: "Serial Number",
+              url: "serial/{letterCase}",
+              defaults: new { controller = "Home", action = "Serial", letterCase = "upper" }
+          );
+            // Parameter cannot be passed like /person_name/person_1
+            // but it can be passed like /person_name?person=person_1
+            routes.MapRoute(
+              name: "Person\'s Name",
+              url: "person_name",
+              defaults: new { controller = "Home", action = "PersonName" }
+          );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ AutomatedTellerMachine/AutomatedTellerMachine/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutomatedTellerMachine.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Foo()
+        {
+            return View("about");
+        }
+
+        public ActionResult Serial(string letterCase)
+        {
+            var serial = "ASPNETMVC5ATM1";
+            if (letterCase == "lower")
+            {
+                return Content(serial.ToLower());
+            }
+            return Content(serial);
+        }
+
+        public ActionResult PersonName(string person)
+        {
+            var person_1 = "Jorge Monge";
+            var person_2 = "Marta Monge";
+            var person_3 = "Ricardo Monge";
+            if (person == "person_1")
+            {
+                return Content(person_1);
+            }
+            else if (person == "person_2")
+            {
+                return Content(person_2);
+            }
+            return Content(person_3);
+        }
+    }
+}
